7c7d03cbd890c9dec0ab93dbce16d59c7763c086
Scott Karn <andrew.s.karn@gmail.com>
2023-12-02 22:31:34 -0800

    finish 2.1

---
+++ 02/part_one.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+def valid_games_sum(str, maximum = { red: 0, green: 0, blue: 0 })
+  valid_games_ids = []
+  str.lines.map do |line|
+    game_id = line.scan(/\d+/).first
+    can_add = true
+    line.scan(/\d+ \w+/).map do |round|
+      count, color = round.split
+      if maximum[color.to_sym] < count.to_i
+        can_add = false
+        break
+      end
+    end
+    valid_games_ids << game_id if can_add
+  end
+  valid_games_ids
+end
+
+input = File.read('input.txt')
+p valid_games_sum(input, { red: 12, green: 13, blue: 14 }).sum(&:to_i) # 2720
