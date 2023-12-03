301dfa8d99bdb360962ac7b2be621e6f33dd0619
Scott Karn <andrew.s.karn@gmail.com>
2023-12-03 10:34:39 -0800

    part one, terse

--- 02/part_one.rb
+++ 02/part_one.rb
@@ -1,20 +1,11 @@
 # frozen_string_literal: true
 def valid_games_sum(str, color_counts)
-  valid_games_ids = []
   str.lines.map do |line|
-    game_id = line.scan(/\d+/).first
-    can_add = true
-    line.scan(/\d+ \w+/).map do |round|
-      count, color = round.split
-      if color_counts[color.to_sym] < count.to_i
-        can_add = false
-        break
-      end
-    end
-    valid_games_ids << game_id if can_add
-  end
-  valid_games_ids
+    game_id = line[/\d+/]
+    valid = line.scan(/\d+ \w+/).all? { |round| count, color = round.split; count.to_i <= color_counts[color.to_sym] }
+    valid ? game_id.to_i : 0
+  end.sum
 end
 
 input = File.read('input.txt')
-p valid_games_sum(input, { red: 12, green: 13, blue: 14 }).sum(&:to_i) # 2720
+p valid_games_sum(input, { red: 12, green: 13, blue: 14 }) # 2720
